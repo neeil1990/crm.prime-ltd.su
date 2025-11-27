57301aa63f182d3bcf9b87e4ec0511b232874d67
neeil1990 <neeil@mail.ru>
2025-11-27 16:41:09 +0300

    Update task_view_data.php

--- app/Views/tasks/task_view_data.php
+++ app/Views/tasks/task_view_data.php
@@ -463,6 +463,6 @@ if ($total_sub_tasks) {
 <?php if ($login_user->user_type === "staff") { ?>
     <div class="box-title"><span ><?php echo app_lang("activity"); ?></span></div>
     <div class="pl15 pr15 mt15 list-container project-activity-logs-container">
-        <?php // echo activity_logs_widget(array("limit" => 20, "offset" => 0, "log_type" => "task", "log_type_id" => $model_info->id)); ?>
+        <?php echo activity_logs_widget(array("limit" => 20, "offset" => 0, "log_type" => "task", "log_type_id" => $model_info->id)); ?>
     </div>
 <?php } ?>
